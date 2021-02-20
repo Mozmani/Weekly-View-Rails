bdcdbeaf97f880b983a521d63dcccde0bcc09322
Mark <SwiftTactics@gmail.com>
2021-02-20 06:23:43 -0500

    validate calendar start and end times within correct ranges

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -4,6 +4,9 @@ class Calendar < ApplicationRecord
     
     validates :name, presence: true 
     validates :user_id, presence: true
+    validates :wake_hour, presence: true, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 23.5}
+    validates :sleep_hour, presence: true, numericality: {greater_than: :wake_hour, less_than_or_equal_to: 23.5}
+
     belongs_to :user, class_name: "User"
     has_many :tasks, foreign_key: :calendar_id
 end
